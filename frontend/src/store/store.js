81d4a0e35a5064ced22d845765bda7fbb34038f2
Vaishnavi Raghavan <vaishragh03@gmail.com>
2026-05-24 22:57:32 +0530

    Remove duplicate files and unused code; finalize project for submission

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './authSlice';
 import cartReducer from './cartSlice';
-import recommendationReducer from './recommendationSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    cart: cartReducer,
-    recommendation: recommendationReducer
+    cart: cartReducer
   }
 });
